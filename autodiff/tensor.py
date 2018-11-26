9251c8a08479619d321da8833ed5fc39bdd53e59
Anjishnu Kumar <anjishnu.kr@gmail.com>
2018-11-26 20:25:16 +0530

    working autodiff library

--- autodiff/tensor.py
+++ autodiff/tensor.py
@@ -1,11 +1,11 @@
 import numpy as np
 from typing import List, NamedTuple, Callable
+import copy
 
 
 class Dependency(NamedTuple):
     tensor : 'Tensor'
     gradient_function : Callable[[np.ndarray], np.ndarray]
-
 
 
 def ensure_array(arrayable_data):
@@ -22,7 +22,8 @@ def ensure_tensor(data):
 
 
 class Tensor(object):
-
+    # To avoid circular imports
+    
     def __init__(self, 
                  data : np.ndarray,
                  requires_grad : bool = True,
@@ -41,7 +42,8 @@ class Tensor(object):
         return self.data.shape
 
     def zero_grad(self) -> None:
-        self.grad = Tensor(np.zeros_like(self.data), requires_grad=False)
+        self.grad = Tensor(np.zeros_like(self.data, dtype=np.float64), 
+                           requires_grad=False)
 
     def backward(self, grad: 'Tensor' = None) -> 'Tensor':
 
@@ -53,20 +55,42 @@ class Tensor(object):
         else:
             grad = ensure_tensor(grad)
 
-        assert self.requires_grad            
-        self.grad.data += grad.data
+        assert self.requires_grad 
+
+        try:
+            self.grad.data += grad.data
+        except Exception as e:
+            print('self.grad.data', self.grad.data)
+            print('grad.data', grad.data)
+            raise e
         for dependency in self.depends_on:
-            backward_grad = dependency.gradient_function(grad)
+            backward_grad = dependency.gradient_function(copy.deepcopy(grad))
             dependency.tensor.backward(backward_grad)
             
     def sum(self):
         return Sum().execute(self)
 
+    
+    @property
+    def data(self) -> np.ndarray:
+        return self._data
+
+
+    @data.setter
+    def data(self, value: np.ndarray) -> None:
+        self._data = value
+        # Setting the data manually means the gradients are invalidated
+        self.grad = None
+
     def add(self, b):
         return Add().execute(self, ensure_tensor(b))
 
+
     def mult(self, b):
         return Multiply().execute(self, ensure_tensor(b))
+
+    def matmul(self, other):
+        return MatrixMultiply().execute(self, ensure_tensor(other))
 
     def neg(self):
         return Neg().execute(self)
@@ -75,7 +99,7 @@ class Tensor(object):
         return Add().execute(self, ensure_tensor(b).neg())
     
     def __repr__(self) -> str:
-        return "Tensor with data={}".format(str(self.data))
+        return "Tensor with shape={}".format(str(self.data.shape))
     
     def __add__(self, other):
         return self.add(ensure_tensor(other))
@@ -83,19 +107,16 @@ class Tensor(object):
     def __iadd__(self, other):
         """ in place add like n += 1 """
         self.data += ensure_tensor(other).data
-        self.grad = None
         return self
 
     def __isub__(self, other):
         """ in place add like n += 1 """
         self.data -= ensure_tensor(other).data
-        self.grad = None
         return self
 
     def __imul__(self, other):
         """ in place add like n += 1 """
         self.data *= ensure_tensor(other).data
-        self.grad = None
         return self
 
     def __radd(self, other):
@@ -115,6 +136,9 @@ class Tensor(object):
 
     def __rsub__(self, other):
         return Add().execute(ensure_tensor(other), -self)
+
+    def __matmul__(self, other):
+        return self.matmul(other)
 
 
 class Operator(object):
@@ -145,24 +169,29 @@ class UnaryOperator(Operator):
         dependencies = []
         if requires_grad:
             dependency = Dependency(tensor, self._get_grad_fn(tensor))
-            dependencies.append(dependency)
-            
+            dependencies.append(dependency)            
         return Tensor(data, requires_grad, dependencies)
 
 
 class BinaryOperator(Operator):    
 
+    def _get_grad_fn_1(self, *args, **kwargs):
+        return self._get_grad_fn(*args, **kwargs)
+
+    def _get_grad_fn_2(self, *args, **kwargs):
+        return self._get_grad_fn(*args, **kwargs)
+    
     def execute(self, t1: Tensor, t2: Tensor) -> Tensor:
         requires_grad = t1.requires_grad or t2.requires_grad
         data = self._operate(t1, t2)
         dependencies = []
 
         if t1.requires_grad:
-            dependency = Dependency(t1, self._get_grad_fn(t1, t2))
+            dependency = Dependency(t1, self._get_grad_fn_1(t1, t2))
             dependencies.append(dependency)
 
         if t2.requires_grad:
-            dependency = Dependency(t2, self._get_grad_fn(t2, t1))
+            dependency = Dependency(t2, self._get_grad_fn_2(t1, t2))
             dependencies.append(dependency)
             
         return Tensor(data, requires_grad, dependencies)
@@ -195,25 +224,73 @@ class Neg(UnaryOperator):
 class Add(BinaryOperator):
 
     def _operate(self, t1: Tensor, t2: Tensor):
-        assert(t1.data.shape == t2.data.shape)
         data = t1.data + t2.data
         return data
 
+    def _get_grad_fn_2(self, t1, t2):
+        return self._get_grad_fn(t2, t1)
+
+
     def _get_grad_fn(self, t1 : Tensor, t2 : Tensor):
+
         def grad_fn(grad : np.ndarray) -> np.ndarray:
+            ndims_added = grad.data.ndim - t1.data.ndim
+            for _ in range(ndims_added):
+                grad.data = grad.data.sum(axis=0)
+                
+            for i, dim in enumerate(t1.data.shape):
+                if dim == 1:
+                    grad.data = grad.data.sum(axis=i, keepdims=True)       
             return grad
+
         return grad_fn
+
+
+def reshape(t1, t2):
+    if t1.shape == (): 
+        t1._data = np.broadcast_to(t1.data, t2.shape) 
+    elif t2.shape == (): 
+        t2._data = np.broadcast_to(t2.data, t1.shape)
+    return t1, t2 
 
 
 class Multiply(BinaryOperator):
     
     def _operate(self, t1: Tensor, t2: Tensor):
-        assert(t1.data.shape == t2.data.shape)
         data = t1.data * t2.data
         return data
 
+    def _get_grad_fn_2(self, t1, t2):
+        return self._get_grad_fn(t2, t1)
+
+
     def _get_grad_fn(self, t1 : Tensor, t2 : Tensor):
+
         def grad_fn(grad : np.ndarray) -> np.ndarray:
-            return grad.data * t2.data 
+            #return grad.data * t2.data
+            ndims_added = grad.data.ndim - t1.data.ndim
+            for _ in range(ndims_added):
+                grad.data = grad.data.sum(axis=0)                
+            for i, dim in enumerate(t1.data.shape):
+                if dim == 1: grad.data = grad.data.sum(axis=i, keepdims=True)
+
+            return grad.data * t2.data
+
         return grad_fn
     
+
+class MatrixMultiply(BinaryOperator):
+
+    def _operate(self, t1: Tensor, t2:Tensor):
+        return t1.data @ t2.data
+
+    def _get_grad_fn_1(self, t1, t2):
+
+        def grad_fn(grad):
+            grad = grad.data @ t2.data.T
+            return grad
+
+        return grad_fn
+ 
+    def _get_grad_fn_2(self, t1, t2):
+        return lambda grad : t1.data.T @ grad.data
--- test_tensor.py
+++ test_tensor.py
@@ -1,4 +1,5 @@
 from autodiff.tensor import Tensor
+import numpy as np
 
 test_registry = {}
 def register_test(func):
@@ -24,6 +25,21 @@ def test_simple_add():
     print('t2.grad.data', t2.grad.data)
     assert t1.grad.data.tolist() == [-1, -2, -3]
 
+@register_test
+def test_simple_add_2():
+
+    t1 = Tensor([[1, 2, 3], [4,5,6]]) # shape=(2, 3)
+    t2 = Tensor([[7, 8, 9]]) # shape=(1, 3)
+
+    t3 = t1 + t2 # shape=(2, 3)
+    t3.backward([[1,1,1], [1,1,1]])
+
+    print('t1.grad.data', t1.grad.data)
+    print('t2.grad.data', t2.grad.data)
+
+    assert t1.grad.data.tolist() == [[1, 1, 1], [1, 1, 1]]
+    assert t2.grad.data.tolist() == [[2, 2, 2]]
+
 
 @register_test
 def test_simple_sub():
@@ -42,26 +58,72 @@ def test_simple_mul():
     t1 = Tensor([1, 2, 3])
     t2 = Tensor([4, 5, 6])
     t3 = t1 * t2
+
     t3.backward([-1., -2., -3.])
     print('t1.grad.data', t1.grad.data)
     print('t2.grad.data', t2.grad.data)
     assert t1.grad.data.tolist() == [-4., -10., -18.]
     assert t2.grad.data.tolist() == [-1., -4., -9.]
 
+
 @register_test
 def minimize_a_function():
     x = Tensor([10, -10, 10, -5, 6, 3, 1])
     # we want to minimize the sum of squares
-
+    
     for i in range(100):
         sum_of_squares = (x*x).sum() # 0-Tensor
         sum_of_squares.backward()
         delta_x = x.grad.data * 0.1 
         x = Tensor(x.data - delta_x)
         if i%20 == 0:
-            print('iteration', i, 'sum_of_squares.data', sum_of_squares.data)
-
+            print('iteration', i, 'sum_of_squares.data', 
+                  sum_of_squares.data)
     assert sum_of_squares.data < 1e-12
+
+@register_test
+def test_matmul():
+    # (3, 2)
+    t1 = Tensor([[1, 2], [3, 4], [5, 6]])
+    # (2, 1)
+    t2 = Tensor([[10], [20]])
+    t3 = t1 @ t2
+    assert t3.data.tolist() == [[50], [110], [170]]
+    grad = Tensor([[-1], [-2], [-3]])
+    t3.backward(grad)
+    np.testing.assert_array_equal(t1.grad.data, 
+                                  grad.data @ t2.data.T)
+        
+@register_test
+def test_learned_function():
+    x_raw = np.random.randn(100, 3)
+    coef = np.array([-1, + 3, -2], dtype=np.float64)
+    y_raw = x_raw @ coef + 5 + np.random.randint(-2, 2, size=(100,))
+    x_data = Tensor(x_raw, requires_grad=False)
+    y_data = Tensor(y_raw, requires_grad=False)    
+    w = Tensor(np.random.randn(3))
+    b = Tensor(np.random.randn())
+
+
+    print ('x_raw', x_raw)
+    print ('y_raw', y_raw)
+
+    for epoch in range(100):
+
+        w.zero_grad()
+        b.zero_grad()
+
+        predicted = (x_data @ w) + b
+        errors = predicted - y_data
+
+        loss = (errors * errors).sum()
+        loss.backward()
+
+        w -= w.grad * 0.001
+        b -= b.grad * 0.001
+
+        if not epoch % 20:
+            print(epoch, 'loss', loss.data)
 
 
 for name, test in test_registry.items():
